4373cdec6713313f0bc98637fccb01dae984df48
i314q159 <i314q159@outlook.com>
2024-01-10 17:15:29 +0800

    update

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from xml.dom import minidom
 
 
 def get_path(path):
-    with open(path, 'r') as f:
+    with open(path, "r") as f:
         lines = f.readlines()
     return lines
 
@@ -19,17 +19,17 @@ def clean_path(lines):
 def get_xml(arr):
     doc = minidom.Document()
 
-    manifest_node = doc.createElement('manifest')
+    manifest_node = doc.createElement("manifest")
     doc.appendChild(manifest_node)
 
     for item in arr:
-        project_node = doc.createElement('project')
+        project_node = doc.createElement("project")
 
-        path_attribute = doc.createAttribute('path')
+        path_attribute = doc.createAttribute("path")
         path_attribute.value = item
         project_node.setAttributeNode(path_attribute)
 
-        name_attribute = doc.createAttribute('name')
+        name_attribute = doc.createAttribute("name")
         name_attribute.value = item
         project_node.setAttributeNode(name_attribute)
 
@@ -38,10 +38,10 @@ def get_xml(arr):
     return doc.toprettyxml(indent="\t")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     file_path = f"./8mp_android11_path.txt"
     manifest_paths = clean_path(get_path(file_path))
 
     xml_str = get_xml(manifest_paths)
-    with open('output.xml', 'w') as f:
+    with open("default.xml", "w") as f:
         f.write(xml_str)
--- readme.md
+++ readme.md
@@ -1,5 +1,9 @@
 # gerrit_path
 
++ 用find_git.sh > 8mp_android11_path.txt，获得形如./packages/modules/IPsec/.git这样格式的路径
++ 用gm.py生成xml里面的project节点的xml文件
++ 拼接完整的xml文件，加入remote和default节点，fetch是相对于于gerrit存放裸仓库的位置
+
 ```xml
 <?xml version="1.0" encoding="UTF-8"?>
 <manifest>
@@ -9,7 +13,5 @@
     <default revision="master"
         remote="origin"
         sync-j="4" />
-    <project name="a" path="a" />
-
 </manifest>
 ```
